88a2ca7eebaa6bfbbbb3ce4bfef628d9c617d2ab
macmade <macmade@xs-labs.com>
2017-05-18 23:58:46 +0200

    Revert "Printing last ouptput for undetected errors..."
    This reverts commit 4cba57dd397cc1696bc6a5572463f79faec88208.

--- xrun/Classes/XRXcodeOutputProcessor.h
+++ xrun/Classes/XRXcodeOutputProcessor.h
@@ -42,7 +42,6 @@ NS_ASSUME_NONNULL_BEGIN
 @property( atomic, readonly           ) BOOL       hasAnalyzerWarnings;
 @property( atomic, readonly           ) BOOL       hasStandardErrorOutput;
 @property( atomic, readonly, nullable ) NSString * lastOutput;
-@property( atomic, readonly, nullable ) NSString * lastOutputWithoutMatch;
 @property( atomic, readonly           ) BOOL       errorDetectedOnLastOutput;
 @property( atomic, readonly, nullable ) NSString * standardErrorOutput;
 
